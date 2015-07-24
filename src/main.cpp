b77065d44ad62ee23d98e83454eb51ee1c7fcc9d
Shockk <shokku.ra@gmail.com>
2015-07-24 14:29:21 +0000

    Write initial test main file

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int, char **) {
+	controld::ListenSocket listenSock(controld::Address::Any, controld::Port(1337));
+	while(listenSock) {
+		auto clientSock = listenSock.Accept();
+		controld::Command cmd;
+		cmd << clientSock;
+		std::cout << "received cmd: " << cmd << std::endl;
+	}
+	return 0;
+}
