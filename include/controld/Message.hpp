96054a64fdcdf7ed6d3873b3fb7e0b4df3337b6a
Shockk <shokku.ra@gmail.com>
2015-07-25 14:28:43 +0000

    Message: Add intensities

--- include/controld/Message.hpp
+++ include/controld/Message.hpp
@@ -39,13 +39,26 @@ namespace controld {
 
 	class Message {
 	public:
-		Command command;
+		Command command = Command::None;
+		std::uint16_t intensity[3] = {0, 0, 0};
 
 		Message(const Command command = Command::None) : command(command) {}
 	};
 
 	inline std::ostream & operator <<(std::ostream &os, const Message &message) {
 		os << message.command;
+		switch(message.command) {
+			case Command::SetRed:
+			case Command::SetGreen:
+			case Command::SetBlue:
+				os << ' ' << message.intensity[0];
+				break;
+			case Command::SetAll:
+				os << ' ' << message.intensity[0] << ' ' << message.intensity[1] << ' ' << message.intensity[2];
+				break;
+			default:
+				break;
+		}
 		return os;
 	}
 
